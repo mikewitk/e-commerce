45f10d5124a5db1dfc3cf79f754b09461a1c7eb9
mikewitk <mike.witk@gmail.com>
2024-04-15 20:00:06 -0400

    install tailwind, autoprefixer, postcss
    created config files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.tsx", "./src/**/*.ts", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
